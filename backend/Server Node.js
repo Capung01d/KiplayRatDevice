2754d8d860634d9481fd0547cd8347c0981ecc6d
Capung01d <168819229+Capung01d@users.noreply.github.com>
2026-02-05 20:06:20 +0700

    Create Server Node.js

---
+++ backend/Server Node.js
@@ -0,0 +1,4 @@
+cd backend
+npm init -y          # Bikin package.json (dependency manager)
+npm i express mongoose cors  # Install server library
+node server.js       # JALANKAN C2 SERVER (port 3000)
